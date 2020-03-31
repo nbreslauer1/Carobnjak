ec4ed5d9431c1068739e30eae62f67b699ecda00
nbreslauer1 <62965617+nbreslauer1@users.noreply.github.com>
2020-04-01 00:49:07 +0200

    Add empty project

---
+++ Carobnjak/Assets/Script/Carobnjak.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Carobnjak : MonoBehaviour {
+
+    // Use this for initialization
+    int max;
+    int min;
+    int Pokusaj;
+    int maxBrojPokusaja = 10;
+    public Text text;
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        max = 1001;
+        min = 1;
+        NextPokusaj();
+    }
+
+    public void pokusajLower()
+    {
+        max = Pokusaj;
+        NextPokusaj();
+    }
+    public void pokusajHigher()
+    {
+        min = Pokusaj;
+        NextPokusaj();
+    }
+    void NextPokusaj()
+    {
+        Pokusaj = Random.Range(min, max + 1);
+        text.text = Pokusaj.ToString();
+        maxBrojPokusaja = maxBrojPokusaja - 1;
+        if (maxBrojPokusaja <= 0)
+        {
+            Application.LoadLevel("Win");
+        }
+    }
+}
